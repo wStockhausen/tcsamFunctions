ece09726796fe7e070d51a21863ddaf2852bf211
wStockhausen <william.stockhausen@noaa.gov>
2018-12-17 06:22:15 -0800

    Incremented version to 2018.12.14. Added "undetermined" maturity state to calc.WatZ for males.

--- R/calc.WatZ.R
+++ R/calc.WatZ.R
@@ -5,12 +5,12 @@
 #'   based on 2015 NMFS survey.
 #'   
 #'@param z : vector of sizes (mm CW)
-#'@param sex : 'MALE' or 'FEMALE'
-#'@param maturity : 'IMMATURE' or 'MATURE'
+#'@param sex : vector of sexes ('MALE' or 'FEMALE')
+#'@param maturity : vector of maturity states ('IMMATURE', 'MATURE', or 'UNDETERMINED')
 #'@param male : list of regression coefficients for immature, mature crab (see details)
-#'@param male : list of regression coefficients for immature, mature crab (see details)
+#'@param female : list of regression coefficients for immature, mature crab (see details)
 #'
-#'@return a vector of weights corresponding to the input sizes
+#'@return a vector of weights corresponding to the input sizes, sexes, and maturity states
 #'
 #'@details 'male' and 'female' are lists with named elements 'a' and 'b', whose values
 #'reflect the parameters in the eq: w = a*z^b
@@ -19,18 +19,22 @@
 #'
 #-----------------------------------------------------------
 calc.WatZ<-function(z,sex,maturity,
-                    male=list(immature=list(a=0.00027,b=3.022134),
-                                mature=list(a=0.00027,b=3.022134)),
+                    male=list(immature    =list(a=0.00027,b=3.022134),
+                              mature      =list(a=0.00027,b=3.022134),
+                              undetermined=list(a=0.00027,b=3.022134)),
                     female=list(immature=list(a=0.000562,b=2.816928),
-                                  mature=list(a=0.000441,b=2.898686))){
-    idx.m<-toupper(sex)==  'MALE';
+                                mature  =list(a=0.000441,b=2.898686))){
+    idx.m<-toupper(sex)=='MALE';
     idx.f<-toupper(sex)=='FEMALE';
     idx.imm<-toupper(maturity)=='IMMATURE';
-    idx.mat<-toupper(maturity)==  'MATURE';
+    idx.mat<-toupper(maturity)=='MATURE';
+    idx.und<-toupper(maturity)=='UNDETERMINED';
     
-    wgt<-(male$immature$a*z^male$immature$b)*(idx.m&idx.imm);
-    wgt<-wgt+(male$mature$a*z^male$mature$b)*(idx.m&idx.mat);
-    wgt<-wgt+(female$immature$a*z^female$immature$b)*(idx.f&idx.imm);
-    wgt<-wgt+(female$mature$a*z^female$mature$b)    *(idx.f&idx.mat);
+    wgt<-(male$immature$a    *z^male$immature$b    )*(idx.m&idx.imm)+
+         (male$mature$a      *z^male$mature$b      )*(idx.m&idx.mat)+
+         (male$undetermined$a*z^male$undetermined$b)*(idx.m&idx.und)+
+         (female$immature$a  *z^female$immature$b  )*(idx.f&idx.imm)+
+         (female$mature$a    *z^female$mature$b    )*(idx.f&idx.mat);
+    
     return(wgt);
 }
